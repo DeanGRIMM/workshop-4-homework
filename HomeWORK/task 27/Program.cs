0b0695b29d668ae8c2701c0f5d6ac09184f55841
Dmitry <dean.grimm@yandex.ru>
2023-01-12 12:16:43 +0800

    задача 27

---
+++ HomeWORK/task 27/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт
+// сумму цифр в числе. Через строку решать нельзя.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+try
+{
+    Console.WriteLine("Введите целое число");
+    int num = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"сумму цифр в числе {num} равно {Sum(num)}");
+}
+catch
+{
+    Console.WriteLine("надо было вводить именно целое число");
+}
+int Sum(int num)
+{
+    int sum = 0;
+    for (int n = num; n > 0; n /= 10)
+        sum += n % 10; 
+    return sum;
+}
+
