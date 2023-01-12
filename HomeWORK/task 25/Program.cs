a8c2d3ccf115ac290be1a255aa2fd10ae3cc3f6c
Dmitry <dean.grimm@yandex.ru>
2023-01-12 11:52:29 +0800

    задача 25

--- HomeWORK/task 25/Program.cs
+++ HomeWORK/task 25/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Использование функций обязательно, начиная с этого ДЗ!
+
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+// # 3, 5 -> 243 (3⁵)
+// # 2, 4 -> 16
+try
+{
+    Console.WriteLine("Введите целое число");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите целое число");
+    int b = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"число {a} в степени {b} равно {Prod(a, b)}");
+}
+catch
+{
+    Console.WriteLine("надо было вводить именно целое число");
+}
+int Prod(int a, int b)
+{
+    int p = a;
+    for (int i=1; i<b; i++)
+        p*=a;
+    return p;
+}
