243f55a93b6a62b4ab74057f2efd1012f7f1d3b5
Dmitry <dean.grimm@yandex.ru>
2023-01-12 13:41:43 +0800

    задача 29

--- HomeWORK/task 29/Program.cs
+++ HomeWORK/task 29/Program.cs
@@ -1,2 +1,31 @@
 ﻿// Напишите программу, которая задаёт массив из 8 целых чисел с клавиатуры 
 // и далее выводит массив на экран в одну строку.
+void FillArray(int[]col)
+{
+    int length = col.Length;
+    int index = 0;
+    while (index<length)
+    {
+        Console.WriteLine("Введите целое число");
+        int num = Convert.ToInt32(Console.ReadLine());        
+        col[index] = num;        
+        index++;
+    }
+}
+void PrintArray(int[]col1)
+{
+    int count = col1.Length;
+    int position = 0;
+    while (position<count)
+    {
+        Console.Write(col1[position]);
+        Console.Write(" ");
+        position++;
+    }
+}
+
+int[] array = new int [8];
+FillArray(array);
+Console.WriteLine();
+PrintArray(array);
+
